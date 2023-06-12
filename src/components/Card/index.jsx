273ce2e9bacad6e0d8eb7ff3569566fc7e51aec6
ElaineLeandro <elainerleandro1@gmail.com>
2023-06-12 20:01:31 -0300

    Add componete Card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,33 @@
+import { Tagging } from "../Tagging";
+import { Container, Title, Icon,Content, Flags} from "./styles";
+import { ImStarFull,ImStarEmpty} from "react-icons/im";
+
+
+    export function Card({title, icon , paragraph }) {
+    return(
+        <Container>
+            
+            <Title>
+                {title}
+            </Title>
+            <Icon>
+                {icon} 
+                
+                {Array(4).fill(<ImStarFull/>)}
+
+                <ImStarEmpty/>               
+            </Icon>
+            <Content>
+                {paragraph}
+            </Content>
+            
+            <Flags>
+            <Tagging tag="Ficção Científica" />
+            <Tagging tag="Drama" />
+            <Tagging tag="Familia" />
+            </Flags>
+            
+
+        </Container>
+    )
+}
---
+++ src/components/Card/styles.js
@@ -0,0 +1,50 @@
+import  styled from "styled-components";
+
+
+export const  Container = styled.div`
+    grid-area: typings;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 200px;
+    padding: 32px;
+    border:none;
+    border-radius: 16px;
+    background-color: ${({theme})=> theme.COLORS.BACKGROUND_500}
+    
+
+`
+export const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom:8px;
+  inline-size: max-content;
+  color: ${({theme})=> theme.COLORS.GRAY_200};
+ 
+`
+export const Icon = styled.div`
+    display: flex;
+    gap: 8px;
+    margin-bottom: 8px;
+
+    >svg{
+        width: 12px;
+        height: 12px;
+        color: ${({theme})=> theme.COLORS.PINK};
+        background-color: ${({theme})=> theme.COLORS.PINK_200};
+        
+    }
+`
+export const Content = styled.span`
+color: ${({theme})=> theme.COLORS.GRAY_300};
+font-weight: 700;
+font-size: 16px;
+line-height: 18px;
+margin-top: 15px;
+margin-bottom: 15px;`
+;
+
+export const Flags = styled.div`
+    display: flex;
+    gap: 8px;
+    
+`
