5c0da696a312cc65311e909556a2f76ed7cd579f
ElaineLeandro <elainerleandro1@gmail.com>
2023-07-03 11:30:39 -0300

    Página MoviePreview layout.

--- src/pages/MoviePreview/index.jsx
+++ src/pages/MoviePreview/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Content, ContentTitle,Icon,Tags, ContentDateTime} from "./styles";
+import { Container, Content, ContentTitle,Icon,Tags, ContentDateTime, Description} from "./styles";
 import { Header } from "../../components/Header";
 import {BiArrowBack, BiTime} from "react-icons/bi"
 import { ImStarFull,ImStarEmpty} from "react-icons/im";
@@ -27,13 +27,14 @@ export function MoviePreview({Icon: icon}){
             </ContentTitle>
 
             <ContentDateTime>
-               <span className="avatar">
-                 Por Elaine Leandro
+               <span>
                 <img src="https://github.com/ElaineLeandro.png" alt="Avatar do usúario" />
+                 Por Elaine Leandro
                 </span>
-                <span className="time">
-                    <BiTime/>
-                </span>
+                <strong>
+                    <BiTime color="#22d3ee" size={20}/>
+                     23/05/22 às 08:00
+                </strong>
         
 
             </ContentDateTime>
@@ -48,10 +49,23 @@ export function MoviePreview({Icon: icon}){
                tag="Famila"
             />
             </Tags>
+
+            <Description>
+            
+            <p>Um dos aspectos mais interessantes do filme é que ele conta a mesma narrativa a partir de vários pontos de vista. 
+            Embora August seja o protagonista, conseguimos perceber que o enredo afeta todos à sua volta: a mãe que parou de trabalhar, a irmã que não tem atenção, etc.
+            Isso nos ajuda a perceber que cada história tem, pelo menos, duas versões. Na visão de Auggie, Jack fingiu ser seu amigo, mas nunca gostou dele.
+            Quando assistimos à sua versão dos fatos, percebemos que ele também era discriminado por ter menos dinheiro que os colegas e que estava tentando 
+            se "enturmar" quando fez piadas sobre o garoto novo.</p>
+            <p>Na escola, a turma está estudando preceitos e reflete sobre a antiga citação egípcia: “seus feitos são seus monumentos”.
+            Ela significa que aquilo que mais importa, e o motivo pelo qual somos lembrados, são as ações que praticamos.
+            Mais do que aquilo que pensamos ou dizemos, é o que fazemos pelos outros que pode transformar o mundo.
+            Auggie está totalmente isolado dos colegas e sofre bullying de um deles, Julian. Na prova de Ciências, percebe que Jack Will,
+            o colega do lado, não sabe as respostas edá cola para ele: desse ato nasce uma amizade. Mais para a frente, Auggie escuta Jack
+            falando mal dele com o resto da classe e fica sozinho de novo.</p> 
+            </Description>
            
-
-
-             </Container>
+            </Container>
         </>
        
     )
--- src/pages/MoviePreview/styles.js
+++ src/pages/MoviePreview/styles.js
@@ -3,25 +3,33 @@ import styled from 'styled-components'
 export const Container = styled.div`
    width: 80%;
    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-areas: "header header"
-    "Content Content"
-    "ContentTitle "
-    ;
+   grid-template-columns:1fr;
+   
+   grid-template-areas: 
+    " header"
+    "content"
+    "title"
+    "datetime"
+    "moviestags"
+    "description"
+   ;
+   
+    padding: 24px;
     margin: 0 auto;
     font-family:'Roboto Slab', serif;
     background-color:$({(theme)}=> theme.COLORS);
 
-    overflow-y: auto;
     font-family: 'Roboto Slab';
 
-    scrollbar-color: #FF859B transparent;
+    overflow-y: auto;
+    height: 40vh;
+    scrollbar-color: #22d3ee transparent;
 
     &::-webkit-scrollbar {
-      width: 10px;
-      border-radius: 10px;
+    width: 10px;
+    border-radius: 10px;
     }
-   
+
     &::-webkit-scrollbar-thumb {
       background-color: ${({theme})=> theme.COLORS.CYAN_400};
       border-radius: 10px;
@@ -32,19 +40,20 @@ export const Container = styled.div`
     border-radius: 3px;
     }
 
-
-`
+`;
 
 export const Content = styled.div`
+  grid-area:content;
   color: ${({theme})=> theme.COLORS.CYAN_500};
   font-size:20px;
   display:flex;
   align-items:center;
   gap: 8px; 
 
-`
+`;
 
 export const ContentTitle = styled.div`
+  grid-area: title;
   display:flex;
   align-items:center;
   justify-content: flex-start;
@@ -67,17 +76,63 @@ export const Icon = styled.div`
       color: ${({theme})=> theme.COLORS.CYAN_400};
         
         
-    }
+    };
 `
 
 export const ContentDateTime = styled.div`
+ grid-area: datetime;
  display: flex;
+ margin-top: 24px;
+ font-size:16px;
+ gap: 8px;
+ font-family:'Roboto',sans-serif;
+ 
+ >span{
+    display: flex;
+    justify-content: center;
+    gap: 8px;
+
+ };
+
+ >span>img{
+    
+    border-radius: 8px;
+    height: 20px;
+    width: 20px;
+    
+
+ };
+
+ >strong{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  
+ };
+ 
 
 `
 export const Tags = styled.div`
+  grid-area: moviestags;
   display: flex;
   margin-top: 40px;
+  margin-bottom: 40px;
   gap: 8px;
   font-size: 12px;
   font-family: 'Roboto';
+`
+
+export const Description = styled.p`
+grid-area: description;
+display: flex;
+flex-direction: column;
+
+>p{
+  margin-top:10px;
+}
+
+
+ 
+ 
 `
